6fdf174ef7aa099e56bc9773152591fe5d2d83d9
manmorjim <manmorjim@gmail.com>
2019-04-14 15:16:12 +0200

    Learning how to manage lists

---
+++ lists.hs
@@ -0,0 +1,35 @@
+hello = ['h', 'e', 'l', 'l', 'o']
+world = "world"
+
+(hello ++ " " ++ world) == (hello ++ [' '] ++ world)
+
+'h':world
+
+
+world !! 1 
+
+head hello == 'h'
+
+tail world
+
+'h':(tail hello) == hello
+
+init hello
+
+last world == 'd'
+
+null (tail ['x']) == True
+
+take (length hello - 1) hello == init hello
+
+drop 1 hello == tail 
+
+(world !! 1 `elem` hello) == True
+
+take 3 (cycle ("beatle" ++ ['j','u','i','c','e']))
+take 3 (repeat ("beatle" ++ ['j','u','i','c','e']))
+
+[x*2 | x <- [1..10]]
+[x*2 | x <- [1..10], x*2 >= 12]
+
+length' xs = sum [1 | _ <- xs]
